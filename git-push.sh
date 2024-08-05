1177484b1eafa269debfa01f46bf81ce583e2717
Johnny Guo <40372255+GUOYUHAN@users.noreply.github.com>
2024-08-05 14:35:06 +0800

    Update git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
-# 获取本地分支列表
-branches=$(git branch | cut -c 3-)
+# 当任何一行命令执行失败时，脚本立即退出
+set -e
 
-# 从分支列表中选择要推送的分支
-echo "请选择要推送的分支："
-select branch in $branches; do
-  # 检查用户是否选择了分支
-  if [ -n "$branch" ]; then
-    # 推送分支
-    echo "$branch"
-    git push origin "$branch"
-    break
-  else
-    echo "无效的选择，请重新选择"
-  fi
-done
+# 先添加所有更改到暂存区
+git add .
+
+# 提示用户输入commit信息
+echo -e "\e[32m\e[1m请输入commit信息："
+read commitMessage
+echo -e "\e[32m\e[1m正在commit..."
+
+# 执行commit操作
+git commit -m "$commitMessage"
+
+# 获取本地分支列表
+# branches=$(git branch | cut -c 3-)
+
+# 获取当前分支名称
+currentBranch=$(git rev-parse --abbrev-ref HEAD)
+
+# 直接推送当前分支
+echo -e "\e[32m\e[1m正在推送分支：\e[0m\e[1m\e[42m$currentBranch\e[0m\e[1m\e[32m..."
+git push origin "$currentBranch"
+
+echo -e "\e[32m\e[1m ______    __    __     _____    _____     _____   ______   ______  
+/ ____/\  /\_\  /_/\   /\ __/\  /\ __/\  /\_____\ / ____/\ / ____/\ 
+) ) __\/ ( ( (  ) ) )  ) )__\/  ) )__\/ ( (_____/ ) ) __\/ ) ) __\/ 
+ \ \ \    \ \ \/ / /  / / /    / / /     \ \__\    \ \ \    \ \ \   
+ _\ \ \    \ \  / /   \ \ \_   \ \ \_    / /__/_   _\ \ \   _\ \ \  
+)____) )   ( (__) )    ) )__/\  ) )__/\ ( (_____\ )____) ) )____) ) 
+\____\/     \/__\/     \/___\/  \/___\/  \/_____/ \____\/  \____\/  
+                                                                    
+\e[0m"
+
+# # 从分支列表中选择要推送的分支
+# echo "请选择要推送的分支："
+# select branch in $branches; do
+#   # 检查用户是否选择了分支
+#   if [ -n "$branch" ]; then
+#     # 推送分支
+#     echo "$branch"
+#     git push origin "$branch"
+#     break
+#   else
+#     echo "无效的选择，请重新选择"
+#   fi
+# done
